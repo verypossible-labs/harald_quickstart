293bb26d71d4545c8c2e9b791f90caa68d1f8a38
Justin Schneck <jschneck@mac.com>
2019-10-19 15:14:46 -0400

    Remove nerves_firmware_ssh keys

--- config/target.exs
+++ config/target.exs
@@ -4,25 +4,6 @@ use Mix.Config
 # See https://hexdocs.pm/nerves_firmware_ssh/readme.html for more information
 # on configuring nerves_firmware_ssh.
 
-keys =
-  [
-    Path.join([System.user_home!(), ".ssh", "id_rsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ecdsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ed25519.pub"])
-  ]
-  |> Enum.filter(&File.exists?/1)
-
-if keys == [],
-  do:
-    Mix.raise("""
-    No SSH public keys found in ~/.ssh. An ssh authorized key is needed to
-    log into the Nerves device and update firmware on it using ssh.
-    See your project's config.exs for this error message.
-    """)
-
-config :nerves_firmware_ssh,
-  authorized_keys: Enum.map(keys, &File.read!/1)
-
 config :nerves_runtime, :kernel, use_system_registry: false
 
 # Configure nerves_init_gadget.
